ac39044c334b60d635a374424a216496e4881ecc
Kyle Freemantle <kcfreemantle@gmail.com>
2023-04-17 17:11:34 -0700

    lab 11 complete, tests running and app runs in the console

---
+++ driver/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Import the driver handler module (it will automatically start listening for events)
+require('./handler.js');
---
+++ vendor/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Import the simulatePickup function from the handler
+const simulatePickup = require('./handler.js');
+
+// Trigger a simulated package pickup event for a given store name
+simulatePickup('1-206-flowers');
